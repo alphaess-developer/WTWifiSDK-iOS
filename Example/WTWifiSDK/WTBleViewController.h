e8d5943aeeb49496271c5eba75e9e3f51e6c47d7
Cavan <cavan@alpha-ess.com>
2024-04-18 15:17:41 +0800

    Update Demo Project.

---
+++ Example/WTWifiSDK/WTBleViewController.h
@@ -0,0 +1,17 @@
+//
+//  WTBleViewController.h
+//  WTWifiSDK_Example
+//
+//  Created by cavan on 2024/4/18.
+//  Copyright © 2024 Cavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WTBleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/WTWifiSDK/WTBleViewController.m
@@ -0,0 +1,32 @@
+//
+//  WTBleViewController.m
+//  WTWifiSDK_Example
+//
+//  Created by cavan on 2024/4/18.
+//  Copyright © 2024 Cavan. All rights reserved.
+//
+
+#import "WTBleViewController.h"
+
+@interface WTBleViewController ()
+
+@end
+
+@implementation WTBleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
