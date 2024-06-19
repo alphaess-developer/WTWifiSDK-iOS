4fb3e6860bebd96a9ae915c53c0c55e29f97353e
Cavan <cavan@alpha-ess.com>
2024-06-19 15:27:34 +0800

    Update ems extend model.

--- WTWifiSDK.xcframework/ios-arm64/WTWifiSDK.framework/Headers/WTUpdateExtendModel.h
+++ WTWifiSDK.xcframework/ios-arm64/WTWifiSDK.framework/Headers/WTUpdateExtendModel.h
@@ -32,34 +32,34 @@ NS_ASSUME_NONNULL_BEGIN
 /// Whether to set grid-connected charging and discharging.
 /// (1 byte, bit 0 corresponds to grid-connected charging, bit 1 corresponds to grid-connected discharging, bit 2 indicates daily setting when 0, and weekly setting when 1)
 @property (nonatomic , copy) NSString              * TIMEChaDisEN;
-@property (nonatomic , copy) NSString              * TIMEChaDis1;
-@property (nonatomic , copy) NSString              * TIMEChaDis2;
-@property (nonatomic , copy) NSString              * TIMEChaDis3;
-@property (nonatomic , copy) NSString              * TIMEChaDis4;
-@property (nonatomic , copy) NSString              * TIMEChaDis5;
-@property (nonatomic , copy) NSString              * TIMEChaDis6;
-@property (nonatomic , copy) NSString              * TIMEChaDis7;
-@property (nonatomic , copy) NSString              * TIMEChaDis8;
-@property (nonatomic , copy) NSString              * TIMEChaDis9;
-@property (nonatomic , copy) NSString              * TIMEChaDis10;
-@property (nonatomic , copy) NSString              * TIMEChaDis11;
-@property (nonatomic , copy) NSString              * TIMEChaDis12;
-@property (nonatomic , copy) NSString              * TIMEChaDis13;
-@property (nonatomic , copy) NSString              * TIMEChaDis14;
-@property (nonatomic , copy) NSString              * TIMEChaDis15;
-@property (nonatomic , copy) NSString              * TIMEChaDis16;
-@property (nonatomic , copy) NSString              * TIMEChaDis17;
-@property (nonatomic , copy) NSString              * TIMEChaDis18;
-@property (nonatomic , copy) NSString              * TIMEChaDis19;
-@property (nonatomic , copy) NSString              * TIMEChaDis20;
-@property (nonatomic , copy) NSString              * TIMEChaDis21;
-@property (nonatomic , copy) NSString              * TIMEChaDis22;
-@property (nonatomic , copy) NSString              * TIMEChaDis23;
-@property (nonatomic , copy) NSString              * TIMEChaDis24;
-@property (nonatomic , copy) NSString              * TIMEChaDis25;
-@property (nonatomic , copy) NSString              * TIMEChaDis26;
-@property (nonatomic , copy) NSString              * TIMEChaDis27;
-@property (nonatomic , copy) NSString              * TIMEChaDis28;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis1;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis2;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis3;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis4;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis5;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis6;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis7;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis8;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis9;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis10;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis11;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis12;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis13;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis14;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis15;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis16;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis17;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis18;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis19;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis20;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis21;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis22;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis23;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis24;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis25;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis26;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis27;
+@property (nonatomic , copy) NSArray<NSNumber *>              * TIMEChaDis28;
 @property (nonatomic , copy) NSString              * Channel1;
 @property (nonatomic , copy) NSString              * ControlMode1;
 @property (nonatomic , copy) NSString              * Date1;
@@ -71,7 +71,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic , copy) NSString              * Pause1;
 @property (nonatomic , copy) NSString              * FeedMode1;
 @property (nonatomic , copy) NSString              * FeedValue1;
-@property (nonatomic , copy) NSString              * DryTime1;
+@property (nonatomic , copy) NSArray              * DryTime1;
 @property (nonatomic , copy) NSString              * Channel2;
 @property (nonatomic , copy) NSString              * ControlMode2;
 @property (nonatomic , copy) NSString              * ChargeMode2;
@@ -84,7 +84,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic , copy) NSString              * FeedMode2;
 @property (nonatomic , copy) NSString              * FeedValue2;
 @property (nonatomic , copy) NSString              * SGEnable2;
-@property (nonatomic , copy) NSString              * DryTime2;
+@property (nonatomic , copy) NSArray              * DryTime2;
 
 @end
 
